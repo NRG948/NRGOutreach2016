e78bcb9dfc617366a609b2c02447826201df95ec
NRG Driver Station <chuwei0807@gmail.com>
2018-01-13 14:24:02 -0800

    Add missing file from commit bd59672adbfa40a0dae70231e5f6db9d02a4d552

--- src/org/usfirst/frc/team948/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team948/robot/subsystems/Drive.java
@@ -11,19 +11,31 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Drive extends Subsystem {
 	private static boolean driveEnabled = true;
+	public static final double SCALE_HIGH = 1.0;
+	public static final double SCALE_LOW = 0.5;
+	
     private static final double a = 0.6;
+    private double scale = SCALE_LOW;
+    
+    
+    public void setScale(double s) {
+    	scale = s;
+    }
   
     public void initDefaultCommand() {
         setDefaultCommand(new DefaultDrive());
     }
     
+    
       public void rawDrive(double velX, double velY, double omega){
     	if (driveEnabled) {
-    		velY *= 0.7;
-    		RobotMap.talonRF.set(-(velX+velY+omega*a));
-    		RobotMap.talonRB.set(-(velY-velX+omega*a));
-    		RobotMap.talonLB.set((velX+velY-omega*a));
-    		RobotMap.talonLF.set(velY-velX-omega*a);
+    		velY *= scale;
+    		velX *= scale;
+    		RobotMap.mecanumDrive.driveCartesian(velX, velY, omega);
+//    		RobotMap.talonRF.set(-(velX+velY+omega*a));
+//    		RobotMap.talonRB.set(-(velY-velX+omega*a));
+//    		RobotMap.talonLB.set((velX+velY-omega*a));
+//    		RobotMap.talonLF.set(velY-velX-omega*a);
     	}
     	else {
     		RobotMap.talonRF.set(0);

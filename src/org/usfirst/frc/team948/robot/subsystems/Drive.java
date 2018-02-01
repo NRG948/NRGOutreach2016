b26dfc00c7f9bf2c731f5c44b8c1dfc61b1985e3
ChuweyBar <chuwei0807@gmail.com>
2018-01-31 19:15:33 -0800

    Added xPosition y Position rotationPosition to Drive
    Calculations are above and commented, they should be displayed on the
    shuffleboard.

--- src/org/usfirst/frc/team948/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team948/robot/subsystems/Drive.java
@@ -11,6 +11,25 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  *
  */
 public class Drive extends Subsystem {
+	
+	// According to RobotDrive.mecanumDrive_Cartesian in WPILib:
+    //
+    // LF =  x + y + rot    RF = -x + y - rot
+    // LR = -x + y + rot    RR =  x + y - rot
+    //
+    // (LF + RR) - (RF + LR) = (2x + 2y) - (-2x + 2y)
+    // => (LF + RR) - (RF + LR) = 4x
+    // => x = ((LF + RR) - (RF + LR))/4
+    //
+    // LF + RF + LR + RR = 4y
+    // => y = (LF + RF + LR + RR)/4
+    //
+    // (LF + LR) - (RF + RR) = (2y + 2rot) - (2y - 2rot)
+    // => (LF + LR) - (RF + RR) = 4rot
+    // => rot = ((LF + LR) - (RF + RR))/4
+	public enum Direction {
+		FORWARD, BACKWARD, LEFT, RIGHT
+	}
 	@Override
 	public void periodic() {
 		// TODO Auto-generated method stub
@@ -18,11 +37,29 @@ public class Drive extends Subsystem {
 		SmartDashboard.putData("encoderRF", RobotMap.encoderRF);
 		SmartDashboard.putData("encoderRB", RobotMap.encoderRB);
 		SmartDashboard.putData("encoderLB", RobotMap.encoderLB);
+
+		SmartDashboard.putData("xPosition", Drive.xPosition);
+		SmartDashboard.putData("yPosition", Drive.yPosition);
+		SmartDashboard.putData("rotationPosition", Drive.rotationPosition);
+		
 	}
 
 	private static boolean driveEnabled = true;
 	public static final double SCALE_HIGH = 1.0;
 	public static final double SCALE_LOW = 0.5;
+
+	public static double encoderLF = RobotMap.encoderLF;
+	public static double encoderLB = RobotMap.encoderLB;
+	public static double encoderRF = RobotMap.encoderRF;
+	public static double encoderRB = RobotMap.encoderRB;
+	
+//	 xPos = ((lfEnc + rrEnc) - (rfEnc + lrEnc))*xScale/4.0;
+//    yPos = (lfEnc + lrEnc + rfEnc + rrEnc)*yScale/4.0;
+//    rotPos = ((lfEnc + lrEnc) - (rfEnc + rrEnc))*rotScale/4.0;
+	
+	public static double xPosition = ((encoderLF + encoderRB) - (encoderRF + encoderLB)) / 4.0;
+	public static double yPosition = (encoderLF + encoderLB + encoderRF + encoderRB) / 4.0;
+	public static double rotationPosition = ((encoderLF + encoderLB) - (encoderRF + encoderRB)) / 4.0;
 	
     private static final double a = 0.6;
     private double scale = SCALE_LOW;

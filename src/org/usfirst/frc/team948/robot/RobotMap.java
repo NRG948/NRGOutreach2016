3b5a987a3069ab9498ffbfb8a6cf11824a2c0034
NRG Driver Station <chuwei0807@gmail.com>
2018-02-02 20:39:47 -0800

    Revert "DriveStraightDistance"
    This reverts commit 95c24119673d605fb58edd4607d4c31c715ef021.

--- src/org/usfirst/frc/team948/robot/RobotMap.java
+++ src/org/usfirst/frc/team948/robot/RobotMap.java
@@ -1,19 +1,15 @@
 package org.usfirst.frc.team948.robot;
 
-import org.usfirst.frc.team948.robot.utilities.ContinuousGyro;
-
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import src.org.usfirst.frc948.NRGRobot2018.AHRS;
 
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
@@ -22,8 +18,6 @@ import src.org.usfirst.frc948.NRGRobot2018.AHRS;
  * floating around.
  */
 public class RobotMap {
-	public static AHRS navx;
-	public static ContinuousGyro gyro;
 	public static Victor victorRF;
 	public static Victor victorRB;
 	public static Victor victorLF;
@@ -54,10 +48,6 @@ public class RobotMap {
 		encoderRB = new Encoder(6, 7, false);
 		encoderLF = new Encoder(9, 8, true);
 		encoderLB = new Encoder(2, 3, true);
-		
-		navx = new AHRS(SPI.Port.kMXP);
-		gyro=ContinuousGyro(navx);
 
 	}
-	
 }

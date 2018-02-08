68a032df5a63b8cc001b3971ffaabe80d944867f
SourishMurthy1 <murthysourish31@gmail.com>
2018-02-07 19:02:38 -0800

    Changed signs so the robot doesn't go backwords

--- src/org/usfirst/frc/team948/robot/commands/DefaultDrive.java
+++ src/org/usfirst/frc/team948/robot/commands/DefaultDrive.java
@@ -22,7 +22,7 @@ public class DefaultDrive extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.drive.rawDriveCartesian(-OI.getX(), OI.getY(), OI.getRot());
+    	Robot.drive.rawDriveCartesian(OI.getX(), -OI.getY(), OI.getRot());
     }
 
     // Make this return true when this Command no longer needs to run execute()
